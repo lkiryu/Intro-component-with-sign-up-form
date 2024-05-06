ab3cf4e753bfe62364dd9e078d0ec9f45ce30f22
kiryu <13matheusilva@gmail.com>
2024-05-06 15:29:04 -0300

    info section component add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import InfoSection from './components/info-section/info-section';
+import FormSection from './components/form-section/form-section';
 
 function App() {
   return (
     <main className='container'>
-      
+      <InfoSection/>
+
+      <FormSection/>
     </main>
   );
 }
---
+++ src/components/info-section/info-section.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InfoSection = () => {
+    return (
+        <section className="info">
+            <h2 className="title">Learn to code by watching others</h2>
+            <p className="description">See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
+                but understanding how developers think is invaluable.</p>
+        </section>
+    )
+}
+
+export default InfoSection

7c517b70ef7a9718553eb380ec3d19a4b89df352
kiryu <13matheusilva@gmail.com>
2024-05-07 13:58:46 -0300

    form section style add

--- src/components/form-section/form-section.js
+++ src/components/form-section/form-section.js
@@ -1,28 +1,30 @@
 import React from "react";
 
+import './form-section.css'
+
 const FormSection = () => {
     return(
-        <section className="FormSection">
+        <section className="form-section">
             <p className="try-it-free">Try it free 7 days then $20/mo. thereafter</p>
 
             <form>
-                <div className="input">
-                    <input type="text" name="first-name" placeholder="First name"></input>
-                    <p className="none error">First Name cannot be empty</p>
+                <div className="div-input">
+                    <input className="input" type="text" name="first-name" placeholder="First name"></input>
+                    <p className="none error-text">First Name cannot be empty</p>
                 </div>
-                <div className="input">
-                    <input type="text" name="last-name" placeholder="Last name"></input>
-                    <p className="none error">Last Name cannot be empty</p>
+                <div className="div-input">
+                    <input className="input" type="text" name="last-name" placeholder="Last name"></input>
+                    <p className="none error-text">Last Name cannot be empty</p>
                 </div>
-                <div className="input">
-                    <input type="email" name="email-address" placeholder="Email Address"></input>
-                    <p className="none error">Looks like this is not an email</p>
+                <div className="div-input">
+                    <input className="input" type="email" name="email-address" placeholder="Email Address"></input>
+                    <p className="none error-text">Looks like this is not an email</p>
                 </div>
-                <div className="input">
-                    <input type="password" name="password" placeholder="Password"></input>
-                    <p className="none error">Password cannot be empty</p>
+                <div className="div-input">
+                    <input className="input" type="password" name="password" placeholder="Password"></input>
+                    <p className="none error-text">Password cannot be empty</p>
                 </div>
-                <input type="submit" value="Claim your free trial"></input>
+                <input className="submit" type="submit" value="Claim your free trial"></input>
                 <p className="terms">By clicking the button, you are agreeing to our <a href="/">Terms and Services</a></p>
             </form>
         </section>
